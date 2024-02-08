6f3723c47bcf7909dd40976c4b28f917a833cb30
kilmar <kilmar64@yandex.ru>
2024-02-08 14:55:20 +0300

    Create City model

--- backend/weather/models.py
+++ backend/weather/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class City(models.Model):
+    name = models.CharField(max_length=127)
+    lat = models.FloatField()
+    lon = models.FloatField()
+    last_fetch_time = models.DateTimeField(null=True)
+
+    # Weather fields
+    temp = models.IntegerField(null=True)
+    pressure = models.IntegerField(null=True)
+    wind_speed = models.IntegerField(null=True)
